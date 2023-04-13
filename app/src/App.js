3aeab21da06886e4b4544c2f5f3bda8646dd6ded
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2023-04-12 23:59:58 -0300

    Estilização do Sidebar Search

--- app/src/App.js
+++ app/src/App.js
@@ -3,8 +3,12 @@ import "./App.css";
 import DonutLargeIcon from "@mui/icons-material/DonutLarge";
 import ChatIcon from "@mui/icons-material/Chat";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SearchIcon from "@mui/icons-material/Search";
+import ChatListItem from "./components/ChatListItem/ChatListItem";
 
 const App = () => {
+    const [chatList, setChatList] = React.useState([{}, {}, {}, {}]);
+
     return (
         <div className="app-window">
             <div className="sidebar">
@@ -26,8 +30,25 @@ const App = () => {
                         </div>
                     </div>
                 </header>
-                <div className="search">Search</div>
-                <div className="chat-list">Chat List</div>
+                <div className="search">
+                    <div className="search--input">
+                        <SearchIcon
+                            fontSize="small"
+                            style={{ color: "#919191" }}
+                        />
+                        <input
+                            type="search"
+                            placeholder="Procurar ou começar uma nova conversa"
+                        />
+                    </div>
+                </div>
+                <div className="chat-list">
+                    {chatList.map((chat, key) => (
+                        <ChatListItem
+                            key={key}
+                        />
+                    ))}
+                </div>
             </div>
             <div className="content-area">Content Area</div>
         </div>
---
+++ app/src/components/ChatListItem/ChatListItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ChatListItem.css";
+
+const ChatListItem = () => {
+    return <div>Pocoio</div>;
+};
+
+export default ChatListItem;
