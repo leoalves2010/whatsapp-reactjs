89be71e3406987ea1b1f76278c5a7ccad02ca330
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2023-04-14 20:43:42 -0300

    Estilização do Footer em ContentArea (Chat Window Body)

--- app/src/components/ChatWindow/index.js
+++ app/src/components/ChatWindow/index.js
@@ -3,6 +3,9 @@ import "./styles.css";
 import SearchIcon from "@mui/icons-material/Search";
 import AttachFileIcon from "@mui/icons-material/AttachFile";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import SendIcon from "@mui/icons-material/Send";
+import MicIcon from "@mui/icons-material/Mic";
 
 const ChatWindow = () => {
     return (
@@ -29,7 +32,21 @@ const ChatWindow = () => {
                 </div>
             </div>
             <div className="chatWindow-body"></div>
-            <div className="chatWindow-footer"></div>
+            <div className="chatWindow-footer">
+                <div className="chatWindow-left">
+                    <div className="chatWindow-btn">
+                        <InsertEmoticonIcon style={{ color: "#919191" }} />
+                    </div>
+                </div>
+                <div className="chatWindow-inputArea">
+                    <input type="text" className="chatWindow-input" placeholder="Digite uma mensagem..." />
+                </div>
+                <div className="chatWindow-right">
+                    <div className="chatWindow-btn">
+                        <SendIcon style={{ color: "#919191" }} />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
