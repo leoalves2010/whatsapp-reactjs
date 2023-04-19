b058f11e9164800bd150d940811cd0424c4ca233
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2023-04-18 21:23:32 -0300

    Configuração do Firebase no projeto

---
+++ app/src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyBCTMSYgxTSP6puFfow6G_AKIXoQ7RK7RA",
+    authDomain: "whatsappclone-5a6b7.firebaseapp.com",
+    projectId: "whatsappclone-5a6b7",
+    storageBucket: "whatsappclone-5a6b7.appspot.com",
+    messagingSenderId: "647401395384",
+    appId: "1:647401395384:web:5c5e673079a837e78b504d",
+};
